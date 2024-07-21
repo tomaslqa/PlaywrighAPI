97bac45deedb7a3a68bdaa3c12134285f1a99b55
tomaslqa <ltomas.qa@gmail.com>
2024-07-21 17:25:55 +0200

    Add method to save Users list to test data file

--- global-setup.js
+++ global-setup.js
@@ -1,16 +1,34 @@
-const { request } = require ('@playwright/test');
+const { request } = require('@playwright/test');
 
 
 async function globalSetup(config) {
-    const requestContext = await request.newContext();
-        const response= await requestContext.post(`${process.env.ADMIN_URL}`, {
-          form: {
-            'email': `${process.env.ADMIN_EMAIL}`,
-            'password': `${process.env.ADMIN_PWD}`
-          }
-        });
-    const body = await response.json();
-    process.env.TOKEN = body.data.token;
+  
+  //Saving Authorization token to .env file
+  const requestContext = await request.newContext();
+  const response = await requestContext.post(`${process.env.ADMIN_URL}`, {
+    form: {
+      'email': `${process.env.ADMIN_EMAIL}`,
+      'password': `${process.env.ADMIN_PWD}`
+    }
+  });
+  const body = await response.json();
+  process.env.TOKEN = body.data.token;
+
+  //Fetching and saving all users to testdata file      
+  const requestUsers = await request.newContext();
+  const resp = await requestUsers.get(`${process.env.USERS_URL}`, {
+    headers: {
+      'Authorization': "Bearer " + `${process.env.TOKEN}`,
+    }
+  });
+  const list = await resp.json();
+  const fs = require("fs");
+  fs.writeFile('test-data/testdata_users.json', JSON.stringify(list.data), (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("testdata_users file saved!");
+  });
 }
 
 export default globalSetup;
