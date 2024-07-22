56967a42793560afb79137e8df3f473da109e65a
tomaslqa <ltomas.qa@gmail.com>
2024-07-22 15:06:53 +0200

    Update Home Page with product category link

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -2,10 +2,15 @@ exports.HomePage = class HomePage {
     constructor(page) {
         this.page = page;
         this.loginBtn = page.locator('//button[contains(. ,"LOGIN")]');
+        this.petCleanupLink = page.locator("(//*[contains (text(), 'pet clean-up and odor control')])[1]");
     }
 
     async ClickLoginBtn() {
         await this.loginBtn.click();
+    }
+
+    async OpenPetCleanupCategory() {
+        await this.petCleanupLink.click();
     }
 
 
