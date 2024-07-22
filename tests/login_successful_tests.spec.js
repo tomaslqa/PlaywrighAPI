ed0d73a0522e7fe41becae9e6dad9932fcf3ef21
tomaslqa <ltomas.qa@gmail.com>
2024-07-22 15:43:39 +0200

    Update Sucessfull Login spec file to only one user login

--- tests/login_successful_tests.spec.js
+++ tests/login_successful_tests.spec.js
@@ -3,25 +3,16 @@ import { LoginPage } from '../pages/LoginPage';
 import { HomePage } from '../pages/HomePage';
 const testData = JSON.parse(JSON.stringify(require('../test-data/testdata_users.json')));
 
-test.describe("TC01 Login test for all users", function () {
+test(`TC01 Login with valid credentials for user ${testData[1].first_name} ${testData[1].last_name}`, async ({ page }) => {
 
-    for (const data of testData) {
-        new Promise(resolve => setTimeout(resolve, 40000));
-        test.describe(`Login with valid credentials for user ${data.first_name} ${data.last_name}`, function () {
-            test(`Login for user email ${data.email}`, async ({ page }) => {
-
-                const home = new HomePage(page);
-                const login = new LoginPage(page);
-
-                await page.goto(process.env.URL);
-                await home.ClickLoginBtn();
-                await login.EnterLoginDetails(data.email, process.env.USER_PWD);
-                const responsePromise = page.waitForResponse(resp => resp.url().includes('/api/v1/user/login'));
-                await login.loginBtn.click({ timeout: 1000 });
-                const resp = await responsePromise;
-                await expect(resp.status()).toBe(200);
-            });
-        });
-    }
+    const home = new HomePage(page);
+    const login = new LoginPage(page);
+    console.log("User email: " + testData[1].email);
+    await page.goto(process.env.URL);
+    await home.ClickLoginBtn();
+    await login.EnterLoginDetails(testData[1].email, process.env.USER_PWD);
+    const responsePromise = page.waitForResponse(resp => resp.url().includes('/api/v1/user/login'));
+    await login.loginBtn.click({ timeout: 1000 });
+    const resp = await responsePromise;
+    await expect(resp.status()).toBe(200);
 });
-  
